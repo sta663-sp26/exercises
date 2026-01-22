763df0e26c90b5e8bc18f785a6de5fdf688bbc91
Colin Rundel <rundel@gmail.com>
2026-01-22 12:07:42 -0500

    Updates

--- class_2026-01-22_uv/analytics/models/__init__.py
+++ class_2026-01-22_uv/analytics/models/__init__.py
@@ -1,1 +1,2 @@
 from ..data.cleaner import remove_nulls
+
--- class_2026-01-22_uv/exercises.py
+++ class_2026-01-22_uv/exercises.py
@@ -19,5 +19,23 @@ from analytics.models.regression import LinearModel as lm
 # 4. From within regression.py, import remove_nulls 
 #    using a relative import
 
+# To make this work we needed to add `from ..data.cleaner import remove_nulls` to `regression.py`
+
 import analytics.models.regression as reg
 reg.remove_nulls
+
+# Adding `from ..data.cleaner import remove_nulls` to `__init__.py` is not necessary for this exercise,
+# but it would allow you to import remove_nulls directly from analytics.models if desired.
+
+import analytics.models as models
+models.remove_nulls
+
+
+## Exercise 2
+
+```shell
+cd calculate_ci
+uv init --bare
+uv add numpy scipy --no-workspace # this flag is necessary because the exercises folder is already a workspace (this will not typically be needed)
+uv run python calculate_ci.py
+````
