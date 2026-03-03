1698da6c8487e3d65f5145d38f7e840df924ca17
Colin Rundel <rundel@gmail.com>
2026-03-03 09:24:18 -0500

    Class

--- class_2026-03-03_docker_vetiver/vetiver/api_test.py
+++ class_2026-03-03_docker_vetiver/vetiver/api_test.py
@@ -1,7 +1,7 @@
 import vetiver, pins
+import pandas as pd
 
-
-board = pins.board_local("board/")
+board = pins.board_folder('board', allow_pickle_read=True)
 
 # Load test data
 
@@ -11,7 +11,7 @@ y_test = board.pin_read("mnist_y_test")
 # Test the API
 
 endpoint = vetiver.server.vetiver_endpoint("http://127.0.0.1:8080/predict")
-res = vetiver.server.predict(endpoint, pd.DataFrame(X_test[:10]))
+res = vetiver.server.predict(endpoint, pd.DataFrame(X_test[:10]).to_dict(orient="records"))
 
 print("y_hat = ",res.predict.values)
 print("y     = ",y_test[:10])
